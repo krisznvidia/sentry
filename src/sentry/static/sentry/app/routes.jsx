54376a1aedcda84dc71200cf5554cc839c599149
chrissy <fivefourths@gmail.com>
2017-12-13 14:39:19 -0800

    sew up some stuff left over from git surgery

--- src/sentry/static/sentry/app/routes.jsx
+++ src/sentry/static/sentry/app/routes.jsx
@@ -385,6 +385,7 @@ function routes() {
             component={errorHandler(OrganizationContext)}
           >
             <Route component={errorHandler(OrganizationSettingsLayout)}>
+              {hooksOrgRoutes}
               {orgSettingsRoutes}
             </Route>
 
